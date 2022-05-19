da84ed9a39aa342fb49f79a0b9d204ede68c4db2
jabed366 <jabedhasan231@gmail.com>
2022-05-20 01:12:53 +0600

    First Commit

---
+++ Final/Day 20/structure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define MAX_LEN 100
+#define SIZE 1000
+
+struct Product {
+    int item_no;
+    char item_name[MAX_LEN+1];
+    int on_hand;
+    float price;
+};
+
+
+int main(){
+    int name;
+
+}
